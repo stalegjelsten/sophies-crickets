a5c0ad65af833a792ec90d63bab710767f0559ba
Ståle Gjelsten <stalegjelsten@gmail.com>
2023-06-10 00:40:28 +0200

    working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,71 @@
 <script>
+	import { onMount } from 'svelte';
 	let playing = false;
+	let audio;
+	onMount(() => {
+		audio = new Audio('https://cdn.pixabay.com/download/audio/2021/08/09/audio_9a2f521fc5.mp3');
+	});
 
 	const play = async () => {
-		let audio = await new Audio(
-			'https://cdn.pixabay.com/download/audio/2021/08/09/audio_9a2f521fc5.mp3'
-		);
 		if (playing == true) {
 			audio.pause();
+			playing = false;
 		} else {
 			await audio.play();
+			audio.loop = true;
+			playing = true;
 		}
-		console.log(playing)
-		return !playing
+		console.log(playing);
 	};
-
 </script>
 
-<div id="main">
-	<h1>Sophie says</h1>
-	<button on:click|self|preventDefault={playing = () => play()}>Summ!</button>
-</div>
+<main>
+	<div id="hoved">
+		<h1>Sophie says</h1>
+		<button on:click|self|preventDefault={(playing = () => play())}>Summ!</button>
+	</div>
+</main>
 
 <style>
+	@import url('https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@900&display=swap');
 	:global(body) {
-		height: 100vh;
-		width: 100vw;
 		background: url('wp.jpeg');
 		background-size: cover;
 		background-repeat: no-repeat;
+		overflow: hidden;
 	}
-	#main {
-		text-align: center;
-		margin: auto;
+	main {
+		height: 100vh;
+		width: 100vw;
 		display: flex;
+		flex-direction: column;
+		align-items: center;
+		text-align: center;
+		justify-content: center;
 		vertical-align: center;
 		flex-direction: column;
 	}
 	h1 {
 		color: #fff;
-		font-family: Arial, Helvetica, sans-serif;
+		font-family: 'Red Hat Display', Arial;
+		font-size: 72pt;
 	}
 	button {
-		max-width: 10vw;
-		margin:auto;
+		display: inline-block;
+		outline: 0;
+		cursor: pointer;
+		border-radius: 6px;
+		border: 2px solid #ff4742;
+		color: #fff;
+		background-color: #ff4742;
+		padding: 8px;
+		box-shadow: rgba(0, 0, 0, 0.07) 0px 2px 4px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1.5px 0px;
+		font-weight: 800;
+		font-size: 16px;
+		height: 42px;
+	}
+	button:hover {
+		background: 0 0;
+		color: #ff4742;
 	}
 </style>
